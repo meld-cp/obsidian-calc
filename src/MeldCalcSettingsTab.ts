a0583b9c8fe75ed742b479a59b498bde9b3473a6
Cleon Pinto <cleon@meld.co.nz>
2021-05-31 08:25:23 +1200

    init commit

--- src/MeldCalcSettingsTab.ts
+++ src/MeldCalcSettingsTab.ts
@@ -16,64 +16,14 @@ export default class MeldCalcSettingsTab extends PluginSettingTab {
 
 	// 	containerEl.empty();
 		
-	// 	containerEl.createEl('h2', {text: 'Settings for Meld Encrypt'});
+	// 	containerEl.createEl('h2', {text: 'Settings for Meld Calc'});
 
-	// 	new Setting(containerEl)
-	// 	.setName('Confirm password?')
-	// 	.setDesc('Confirm password when encrypting.')
-	// 	.addToggle( toggle =>{
-	// 		toggle
-	// 			.setValue(this.plugin.settings.confirmPassword)
-	// 			.onChange( async value =>{
-	// 				this.plugin.settings.confirmPassword = value;
-	// 				await this.plugin.saveSettings();
-	// 				this.updateSettingsUi();
-	// 			})
-	// 	})
-	// ;
-
-	// 	new Setting(containerEl)
-	// 		.setName('Remember password?')
-	// 		.setDesc('Remember the last used password for this session.')
-	// 		.addToggle( toggle =>{
-	// 			toggle
-	// 				.setValue(this.plugin.settings.rememberPassword)
-	// 				.onChange( async value =>{
-	// 					this.plugin.settings.rememberPassword = value;
-	// 					await this.plugin.saveSettings();
-	// 					this.updateSettingsUi();
-	// 				})
-	// 		})
-	// 	;
-
-	// 	this.pwTimeoutSetting = new Setting(containerEl)
-	// 		.setName( this.buildPasswordTimeoutSettingName() )
-	// 		.setDesc('The number of minutes to remember the last used password.')
-	// 		.addSlider( slider => {
-	// 			slider
-	// 				.setLimits(0, 120, 5)
-	// 				.setValue(this.plugin.settings.rememberPasswordTimeout)
-	// 				.onChange( async value => {
-	// 					this.plugin.settings.rememberPasswordTimeout = value;
-	// 					await this.plugin.saveSettings();
-	// 					this.updateSettingsUi();
-	// 				})
-	// 			;
-				
-	// 		})
-	// 	;
 
 	// 	this.updateSettingsUi();
 	}
 
 	updateSettingsUi():void{
-		// this.pwTimeoutSetting.setName(this.buildPasswordTimeoutSettingName());
-
-		// if ( this.plugin.settings.rememberPassword ){
-		// 	this.pwTimeoutSetting.settingEl.show();
-		// }else{
-		// 	this.pwTimeoutSetting.settingEl.hide();
-		// }
+		
 	}
 	
 }
--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,94 @@
 import { Notice, Plugin, MarkdownView } from 'obsidian';
-import MeldCalcSettingsTab from './MeldCalcSettingsTab';
+import * as mathjs from 'mathjs';
+//import MeldCalcSettingsTab from './MeldCalcSettingsTab';
+ 
 
-interface MeldClacPluginSettings {
-	// confirmPassword: boolean;
-	// rememberPassword: boolean;
-	// rememberPasswordTimeout: number;
-}
+// interface MeldClacPluginSettings {
+// }
 
-const DEFAULT_SETTINGS: MeldClacPluginSettings = {
-	// confirmPassword: true,
-	// rememberPassword: true,
-	// rememberPasswordTimeout: 30
-}
+// const DEFAULT_SETTINGS: MeldClacPluginSettings = {
+// }
 
 export default class MeldCalcPlugin extends Plugin {
 
-	settings: MeldClacPluginSettings;
+	static evalScope = {};
+
+	//settings: MeldClacPluginSettings;
 
 	async onload() {
 
-		await this.loadSettings();
+		//await this.loadSettings();
 
 		//this.addSettingTab(new MeldCalcSettingsTab(this.app, this));
 
-		// this.addCommand({
-		// 	id: 'encrypt-decrypt',
-		// 	name: 'Encrypt/Decrypt',
-		// 	checkCallback: (checking) => this.processEncryptDecryptCommand(checking, false)
-		// });
+		this.addCommand({
+			id: 'encrypt-calc',
+			name: 'Evaluate',
+			checkCallback: (checking) => this.processEvaluateCommand(checking)
+		});
 
-		// this.addCommand({
-		// 	id: 'encrypt-decrypt-in-place',
-		// 	name: 'Encrypt/Decrypt In-place',
-		// 	checkCallback: (checking) => this.processEncryptDecryptCommand(checking, true)
-		// });
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	processEvaluateCommand(checking: boolean): boolean {
+		const mdview = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (!mdview) {
+			return false;
+		}
+
+		const editor = mdview.sourceMode.cmEditor;
+		if (!editor) {
+			return false;
+		}
+
+		const selection = editor.getSelection();
+
+		let evalText = selection.trim();
+		
+		if ( evalText.length == 0 ) {
+			return false;
+		}
+
+		if ( checking ){
+			return true;
+		}
+
+		let appendResult = false;
+		if ( evalText.endsWith('=') ){
+			appendResult = true;
+			evalText = evalText.slice(0,-1); // remove '='
+		}
+
+		try{
+			const rawResult = mathjs.evaluate( evalText, MeldCalcPlugin.evalScope );
+			//console.log( evalText, mathjs.typeOf(rawResult), rawResult );
+
+			let finalResult = `${rawResult}`;
+			if ( mathjs.typeOf(rawResult) == 'ResultSet' ){
+				finalResult = `${rawResult.entries.last()}`;
+			}
+
+			if ( appendResult ){
+				editor.replaceSelection( `${selection}${finalResult}` );
+			}else{
+				navigator.clipboard.writeText(finalResult).then(()=>{
+					new Notice(finalResult);
+				});
+			}
+		}catch ( ex ){
+			new Notice(ex, 5);
+		}
+
+
+		return true;
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	// async loadSettings() {
+	// 	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	// }
+
+	// async saveSettings() {
+	// 	await this.saveData(this.settings);
+	// }
 
 
 }
