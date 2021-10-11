cc469a8dd49b8888a840c8bc94e6d43773b7dd75
Cleon Pinto <cleon@meld.co.nz>
2021-10-11 13:40:13 +1300

    testing fcal

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { Notice, Plugin, MarkdownView } from 'obsidian';
-import * as mathjs from 'mathjs';
+import { Notice, Plugin, MarkdownView, Editor } from 'obsidian';
+//import * as mathjs from 'mathjs';
+import { Fcal, FcalError } from 'fcal';
 //import MeldCalcSettingsTab from './MeldCalcSettingsTab';
  
 
@@ -12,6 +13,7 @@ import * as mathjs from 'mathjs';
 export default class MeldCalcPlugin extends Plugin {
 
 	static evalScope = {};
+	static fcal = new Fcal();
 
 	//settings: MeldClacPluginSettings;
 
@@ -24,18 +26,18 @@ export default class MeldCalcPlugin extends Plugin {
 		this.addCommand({
 			id: 'encrypt-calc',
 			name: 'Evaluate',
-			checkCallback: (checking) => this.processEvaluateCommand(checking)
+			checkCallback: (checking) => this.processEvaluateCommand_fcal(checking)
 		});
 
 	}
 
-	processEvaluateCommand(checking: boolean): boolean {
+	processEvaluateCommand_fcal(checking: boolean): boolean {
 		const mdview = this.app.workspace.getActiveViewOfType(MarkdownView);
 		if (!mdview) {
 			return false;
 		}
 
-		const editor = mdview.sourceMode.cmEditor;
+		const editor = mdview.editor;
 		if (!editor) {
 			return false;
 		}
@@ -52,35 +54,111 @@ export default class MeldCalcPlugin extends Plugin {
 			return true;
 		}
 
-		let appendResult = false;
-		if ( evalText.endsWith('=') ){
-			appendResult = true;
-			evalText = evalText.slice(0,-1); // remove '='
+		// split in to array of lines
+		const lines = evalText.split('\n');
+		console.log( { lines } );
+
+		let evaluatedLines = [];
+		for (const line of lines) {
+			evaluatedLines.push( this.evaluateLine( line ) );
 		}
 
-		try{
-			const rawResult = mathjs.evaluate( evalText, MeldCalcPlugin.evalScope );
-			//console.log( evalText, mathjs.typeOf(rawResult), rawResult );
+		const formatedResult = evaluatedLines.join('\n');
 
-			let finalResult = `${rawResult}`;
-			if ( mathjs.typeOf(rawResult) == 'ResultSet' ){
-				finalResult = `${rawResult.entries.last()}`;
-			}
-
-			if ( appendResult ){
-				editor.replaceSelection( `${selection}${finalResult}` );
-			}else{
-				navigator.clipboard.writeText(finalResult).then(()=>{
-					new Notice(finalResult);
-				});
-			}
-		}catch ( ex ){
-			new Notice(ex, 5);
-		}
-
+		editor.replaceSelection( formatedResult );
 
 		return true;
 	}
+
+	evaluateLine( line: string ): string {
+		console.log( line );
+		let appendResult = false;
+		let evalLine = line;
+		if ( line.endsWith('=') ){
+			appendResult = true;
+			evalLine = line.slice(0,-1); // remove '='
+		}
+
+		try{
+			console.log( evalLine );
+			const rawResult = MeldCalcPlugin.fcal.evaluate( evalLine );
+			
+			const formatedResult = rawResult.toString();
+			console.log( evalLine, rawResult, formatedResult );
+
+			if ( appendResult ){
+				return `${line}${formatedResult}`;
+			// 	editor.replaceSelection( `${selection}${formatedResult}` );
+			}else{
+			// 	navigator.clipboard.writeText(formatedResult).then(()=>{
+			// 		new Notice(`${formatedResult} (copied)`, 5000);
+			// 	});
+				return line;
+			}
+		}catch ( ex ){
+			if (ex instanceof FcalError) {
+				console.error(ex.message);
+				new Notice(ex.message, 5000);
+			}else{
+				console.error( ex );
+				new Notice(ex, 5000);
+			}
+			return line;
+		}
+	}
+
+	// processEvaluateCommand_mathjs(checking: boolean): boolean {
+	// 	const mdview = this.app.workspace.getActiveViewOfType(MarkdownView);
+	// 	if (!mdview) {
+	// 		return false;
+	// 	}
+
+	// 	const editor = mdview.sourceMode.cmEditor;
+	// 	if (!editor) {
+	// 		return false;
+	// 	}
+
+	// 	const selection = editor.getSelection();
+
+	// 	let evalText = selection.trim();
+		
+	// 	if ( evalText.length == 0 ) {
+	// 		return false;
+	// 	}
+
+	// 	if ( checking ){
+	// 		return true;
+	// 	}
+
+	// 	let appendResult = false;
+	// 	if ( evalText.endsWith('=') ){
+	// 		appendResult = true;
+	// 		evalText = evalText.slice(0,-1); // remove '='
+	// 	}
+
+	// 	try{
+	// 		const rawResult = mathjs.evaluate( evalText, MeldCalcPlugin.evalScope );
+	// 		//console.log( evalText, mathjs.typeOf(rawResult), rawResult );
+
+	// 		let finalResult = `${rawResult}`;
+	// 		if ( mathjs.typeOf(rawResult) == 'ResultSet' ){
+	// 			finalResult = `${rawResult.entries.last()}`;
+	// 		}
+
+	// 		if ( appendResult ){
+	// 			editor.replaceSelection( `${selection}${finalResult}` );
+	// 		}else{
+	// 			navigator.clipboard.writeText(finalResult).then(()=>{
+	// 				new Notice(finalResult);
+	// 			});
+	// 		}
+	// 	}catch ( ex ){
+	// 		new Notice(ex, 5);
+	// 	}
+
+
+	// 	return true;
+	// }
 
 	// async loadSettings() {
 	// 	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
